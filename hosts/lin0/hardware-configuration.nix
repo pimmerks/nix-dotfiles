57f21e3242217a375f36d33e5bd72297eec8c58d
Pim Merks <pimmerks@users.noreply.github.com>
2024-07-12 13:48:21 +0200

    fix: mx-master 3 high resolution scrolling

--- hosts/lin0/hardware-configuration.nix
+++ hosts/lin0/hardware-configuration.nix
@@ -59,4 +59,12 @@
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   hardware.logitech.wireless.enable = true;
+
+  # Disable "high-resolution scrolling" for mx master
+  environment.etc."libinput/local-overrides.quirks".text = ''
+    [Logitech MX Master 3]
+    MatchVendor=0x46D
+    MatchProduct=0x4082
+    AttrEventCode=-REL_WHEEL_HI_RES;-REL_HWHEEL_HI_RES;
+  '';
 }
