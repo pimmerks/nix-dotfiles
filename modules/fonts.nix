440b3e9b09fa8104324bebc924f686a392f274ec
Pim Merks <pimmerks@users.noreply.github.com>
2024-07-15 15:26:02 +0200

    fix: darwin font config

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,35 +1,29 @@
-{ pkgs, user, lib, ... }:
-
-let
-
-  font_options =
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  fonts =
     if lib.strings.hasSuffix "darwin" pkgs.system
     then {
-           fontDir.enable = true;
-           fonts = with pkgs; [
-             (nerdfonts.override {
-               fonts = [ "Meslo" "FiraCode" "JetBrainsMono" ];
-             })
-           ];
-         }
+      packages = with pkgs; [
+        font-awesome
+        (nerdfonts.override {fonts = ["Meslo" "FiraCode" "JetBrainsMono"];})
+      ];
+    }
     else {
-           packages = with pkgs; [
-             dejavu_fonts
-             freefont_ttf
-             gyre-fonts # TrueType substitutes for standard PostScript fonts
-             liberation_ttf
-             unifont
-             noto-fonts-color-emoji
-             noto-fonts
-             noto-fonts-cjk
-             noto-fonts-emoji
-             font-awesome
-             (nerdfonts.override { fonts = [ "Meslo" "FiraCode" "JetBrainsMono" ]; })
-           ];
-         };
-
-in
-
-{
-  fonts = font_options;
+      packages = with pkgs; [
+        dejavu_fonts
+        freefont_ttf
+        gyre-fonts # TrueType substitutes for standard PostScript fonts
+        liberation_ttf
+        unifont
+        noto-fonts-color-emoji
+        noto-fonts
+        noto-fonts-cjk
+        noto-fonts-emoji
+        font-awesome
+        (nerdfonts.override {fonts = ["Meslo" "FiraCode" "JetBrainsMono"];})
+      ];
+    };
 }
