05456dbda3718c222b08d0a3e6714d1cfe121b08
Pim Merks <pimmerks@users.noreply.github.com>
2024-09-18 17:13:04 +0200

    feat: add tmux config

--- home/shell.nix
+++ home/shell.nix
@@ -25,6 +25,64 @@
       nix-direnv.enable = true;
     };
 
+    tmux = {
+      enable = true;
+      baseIndex = 1;
+      escapeTime = 0;
+      historyLimit = 5000;
+      terminal = "xterm-256color";
+      keyMode = "vi";
+      customPaneNavigationAndResize = true;
+
+      # Split and open in current directory.
+      extraConfig = ''
+        bind '"' split-window -c "#{pane_current_path}"
+        bind % split-window -hc "#{pane_current_path}"
+        bind c new-window -c "#{pane_current_path}"
+      '';
+
+      plugins = with pkgs; [
+        {
+          plugin = tmuxPlugins.catppuccin;
+          extraConfig = ''
+            set -g @catppuccin_status_modules_left "null"
+            set -g @catppuccin_status_modules_right "directory session date_time"
+
+            set -g @catppuccin_status_left_separator  ""
+            set -g @catppuccin_status_right_separator " "
+            set -g @catppuccin_status_fill "all"
+            set -g @catppuccin_status_connect_separator "yes"
+
+            set -g @catppuccin_icon_window_last "󰖰"
+            set -g @catppuccin_icon_window_current "󰖯"
+            set -g @catppuccin_icon_window_zoom "󰁌"
+            set -g @catppuccin_icon_window_mark "󰃀"
+            set -g @catppuccin_icon_window_silent "󰂛"
+            set -g @catppuccin_icon_window_activity "󱅫"
+            set -g @catppuccin_icon_window_bell "󰂞"
+
+            set -g @catppuccin_window_default_text "#W"
+            set -g @catppuccin_window_current_text "#W"
+
+            # set -g @catppuccin_window_left_separator "█"
+            # set -g @catppuccin_window_right_separator "█ "
+            # set -g @catppuccin_window_number_position "right"
+            # set -g @catppuccin_window_middle_separator "  █"
+
+            set -g @catppuccin_window_left_separator ""
+            set -g @catppuccin_window_right_separator ""
+            set -g @catppuccin_window_number_position "right"
+            set -g @catppuccin_window_middle_separator ""
+
+            set -g @catppuccin_window_default_fill "number"
+            set -g @catppuccin_window_current_fill "number"
+
+            set -g @catppuccin_directory_text "#{pane_current_path}"
+          '';
+        }
+      ];
+    };
+
     zsh = {
       enable = true;
       autocd = true;
